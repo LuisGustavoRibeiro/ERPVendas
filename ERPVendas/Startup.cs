2bd459be7cca8a524f4222e9e55d456ff4968de5
Luis Gustavo Ribeiro <gustavoribeiro.luis10@gmail.com>
2020-06-27 11:08:46 -0300

    Seeding service

--- ERPVendas/Startup.cs
+++ ERPVendas/Startup.cs
@@ -40,14 +40,17 @@ namespace ERPVendas
                     options.UseMySql(Configuration.GetConnectionString("ERPVendasContext"), builder =>
                         builder.MigrationsAssembly("ERPVendas")));
 
+            services.AddScoped<SeedingService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
