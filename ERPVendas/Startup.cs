b4455da8860ca163f411cf40c9ee791ce4fa1c70
Luis Gustavo Ribeiro <gustavoribeiro.luis10@gmail.com>
2020-07-17 20:50:40 -0300

    Exception para delete e validação dos campos em js e servidor

--- ERPVendas/Startup.cs
+++ ERPVendas/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ERPVendas.Data;
 using ERPVendas.Services;
+using Microsoft.AspNetCore.Localization;
 
 namespace ERPVendas
 {
@@ -50,6 +52,16 @@ namespace ERPVendas
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUs = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUs),
+                SupportedCultures = new List<CultureInfo> { enUs },
+                SupportedUICultures = new List<CultureInfo> { enUs }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
